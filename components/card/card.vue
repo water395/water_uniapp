9f208db35c1afafa628f79774dd4d00ad83a5bb0
water <13432959371@163.com>
2022-07-27 17:43:09 +0800

    init

---
+++ components/card/card.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<button @click="show()">开始</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"card",
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			show(){
+				uni.showToast({
+					title:"加载中..",
+					icon:"loading",
+					duration:3000
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<card color="red" ></card>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
